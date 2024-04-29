06c72e3079130fbeaa74ae065cbc9f6398dc556e
unknown <seyed.mousavi03@gmail.com>
2024-04-29 09:33:37 +0200

    starter files

--- src/iterator/BrowseHistory.java
+++ src/iterator/BrowseHistory.java
@@ -4,21 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BrowseHistory {
-//    private List<String> urls = new ArrayList<>();
-    private String[] urls = new String[10];
+    private List<String> urls = new ArrayList<>();
 
-//    public void push(String url) {
-//        urls.add(url);
-//    }
+    public void push(String url) {
+        urls.add(url);
+    }
 
-//    public String pop() {
-//        var lastIndex = urls.size() - 1;
-//        var lastUrl = urls.get(lastIndex);
-//        urls.remove(lastUrl);
-//        return lastUrl;
-//    }
+    public String pop() {
+        var lastIndex = urls.size() - 1;
+        var lastUrl = urls.get(lastIndex);
+        urls.remove(lastUrl);
+        return lastUrl;
+    }
 
-    public String[] getUrls() {
+    public List<String> getUrls(){
         return urls;
     }
 }
--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -3,13 +3,13 @@ package iterator;
 public class Main {
     public static void main(String[] args) {
         var history = new BrowseHistory();
-//        history.push("a");
-//        history.push("b");
-//        history.push("c");
+        history.push("a");
+        history.push("b");
+        history.push("c");
 
-//        for (var i = 0; i<history.getUrls().size(); i++){
-//            var url = history.getUrls().get(i);
-//            System.out.println(url);
-//        }
+        for (var i = 0; i<history.getUrls().size(); i++){
+            var url = history.getUrls().get(i);
+            System.out.println(url);
+        }
     }
 }
