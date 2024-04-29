8a1d143aebcafd4059b449bfe2ed3cc5516770c5
unknown <seyed.mousavi03@gmail.com>
2024-04-29 07:35:23 +0200

    solution memento

--- src/memento/History.java
+++ src/memento/History.java
@@ -1,2 +1,15 @@
-package memento;public class History {
+package memento;
+
+import java.util.Stack;
+
+public class History {
+    private Stack<EditorState> states= new Stack<>();
+
+    public void push(EditorState state){
+        states.push(state);
+    }
+
+    public EditorState pop() {
+        return states.pop();
+    }
 }
