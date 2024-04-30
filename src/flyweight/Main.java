0251a2d2da97a195bc815a3b2c63c7242d6f5ddf
unknown <seyed.mousavi03@gmail.com>
2024-04-30 10:20:03 +0200

    flyweight solution

--- src/flyweight/Main.java
+++ src/flyweight/Main.java
@@ -2,7 +2,7 @@ package flyweight;
 
 public class Main {
     public static void main(String[] args) {
-        var service = new PointService();
+        var service = new PointService(new PointIconFactory());
         for (var point : service.getPoints()){
             point.draw();
         }
